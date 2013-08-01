177d41c6d94b6b3a6c8677fbec8d01852c1a158d
Troy Shu <tmshu1@gmail.com>
2013-07-31 23:00:26 -0400

    add if save successful then return 200. so still debugging...

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -6,10 +6,13 @@ class IncomingMailsController < ApplicationController
     entry_to_update = HappinessEntry.find(params[:disposable])
     
 	entry_to_update.description = "#{params[:plain]}"
-	entry_to_update.save
+	
 
-    render :text => 'Success', :status => 200
-
+    if entry_to_update.save
+      render :text => 'Success', :status => 200
+    else
+      render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
+    end
 
   end
 end
