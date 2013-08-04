8e981660cc632d672b2e6845debd342ab48747d0
Troy Shu <tmshu1@gmail.com>
2013-08-03 23:01:49 -0400

    parse our happiness score from email

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -5,14 +5,21 @@ class IncomingMailsController < ApplicationController
   def create
 
   	puts "received email, in the create controller"
-  	puts "reply_plain: \n #{params[:reply_plain]}"
+  	#puts "reply_plain: \n#{params[:reply_plain]}"
 
   	#extract happiness entry id
   	entry_id = /.*\+([0-9]+)@/.match(params[:envelope][:to])[1]
 
     entry_to_update = HappinessEntry.find(entry_id)
+
+    reply_plain = parmas[:reply_plain]
+
+    #parse out score
+    reply_plain_matches = /^([0-9]+)\.(.*)/.match(reply_plain)
     
-	entry_to_update.description = "#{params[:reply_plain]}"
+    entry_to_update.score = "#{reply_plain_matches[1]}"
+	entry_to_update.description = "#{reply_plain_matches[2]}"
+
 
     # Do some other stuff with the mail message
 
