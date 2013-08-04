fbb394fe72be9fd9e28a94e4024c8a6659086571
Troy Shu <tmshu1@gmail.com>
2013-08-03 22:14:29 -0400

    mail recieved! fix bug in incoming mails create

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -6,12 +6,6 @@ class IncomingMailsController < ApplicationController
 
   	puts "received email, in the create controller"
 
-    Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    Rails.logger.log params[:html] # print the html decoded body to the logs if present
-    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
-
     entry_to_update = HappinessEntry.find(params[:disposable])
     
 	entry_to_update.description = "#{params[:plain]}"
